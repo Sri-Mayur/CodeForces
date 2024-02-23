df02a91c952390dea483066a29b22749e5a191a6
Sri-Mayur <mayursrivastava877@gmail.com>
2024-02-23 09:05:14 +0530

    If else used

---
+++ Elephant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n <=5){
+        cout<<1;
+    }else if(n % 5 == 0){
+        cout<< n / 5;
+    }else{
+        cout<< (n / 5) + 1;
+    }
+
+    return 0;
+}
